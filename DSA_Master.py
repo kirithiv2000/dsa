82e24e75ec4c70f3ab6faa0bff6a1a4a2107526e
Kirithiv R <52394696+kirithiv2000@users.noreply.github.com>
2024-12-31 22:25:31 +0530

    Update DSA_Master.py

--- DSA_Master.py
+++ DSA_Master.py
@@ -1,2 +1,19 @@
 #comment
-DSA Master
+def binarySearch(array,item):
+    n = len(array)
+   
+    start = 0
+    end = n - 1
+    while start <= end:
+        mid = ( start + end ) // 2
+        if array[mid] == item:
+            return mid
+        elif array[mid]>item:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return -1
+
+
+a = [1,2,3,4,5,6,7,8,9,10]
+print(binarySearch(a,10))
